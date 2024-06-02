85b864c8e609d80572eb3dd8c1f06a686aac0da2
Bassam <bassamelabassy3@gmail.com>
2024-06-02 13:27:30 +0300

    team section

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 $(document).ready(() => {
+  // main slider
     let imgArr = [
       "../images/slider01.jpg",
       "../images/slider02.jpg",
@@ -17,6 +18,68 @@ $(document).ready(() => {
         i = 0;
       }
     }, 5000);
+  
+  // status counter
+
+  let statusValue = $(".status span");
+  const $statusVar = $(statusValue);
+
+  let dataValues = $statusVar.map(function () {
+    return $(this).data('value');
+  }).get()
+
+  let duration = dataValues.map((e) => e * 4);
+  let maxDuration = Math.max(...duration);
+
+  $statusVar.each(function (index) {
+    let init = 0;
+    let x = setInterval(() => {
+      if (init >= dataValues[index] - 1) {
+        clearInterval(x);
+      }
+      init += 1;
+      $(this).html(init);
+    }, maxDuration / dataValues[index]);
+  })
+
+  // team info
+
+  let team = [
+    {
+      imgSrc: "images/team1.png",
+    },
+    {
+      imgSrc: "images/team2.png",
+    },
+    {
+      imgSrc: "images/team3.png",
+    }
+  ];
+
+  let head = $(".volunteers .volunteersMem .content h6");
+
+  for (const [index, value] of Array.from(head).entries()) {
+    $(value).click(function () {
+      let teamInfo = team[index];
+      
+      $(".teamInfo .memCard .img").html(
+        `<img src="${teamInfo.imgSrc}" alt="team1" class="img-fluid">`
+      );
+    })
+  }
+
+  $(".teamInfo").fadeOut();
+  $(".volunteers .volunteersMem .content h6").click(function () {
+    $(".teamInfo").fadeIn(500);
+  });
+
+  $(".memCard").click(function (e) {
+    e.stopPropagation();
+  });
+
+  $(".teamInfo").click(function () {
+    $(this).fadeOut(500);
+  });
 })
 
 
